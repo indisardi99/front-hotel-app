bcf17ed8e1cb929b4aea3c49a0dd5386eef7f780
indisardi99 <indisardi99@gmail.com>
2024-08-23 12:24:36 -0300

    conect back getAllRooms

--- src/app/all-rooms/page.tsx
+++ src/app/all-rooms/page.tsx
@@ -1,22 +1,55 @@
 import RoomCard from "@/components/room-card/room-card";
 
-const page = () => {
-  return (
-    <div className="w-full flex flex-col mt-5 lg:mt-20 p-1 lg:px-32">
-      <RoomCard
-        imageUrl="/room.jpg"
-        title="Suite de Lujo"
-        price="$200 por noche"
-        description="Una suite amplia con vistas al mar y todas las comodidades para una estancia de lujo."
-        services={[
-          { id: "1", title: "WiFi gratuito", icon: "wifi" },
-          { id: "2", title: "Gimnasio", icon: "dumbbell" },
-          { id: "3", title: "Vista al mar", icon: "calendar" },
-          { id: "4", title: "Cama King Size", icon: "leaf" },
-        ]}
-      />
-    </div>
-  );
+interface Room {
+  number: number;
+  price: number;
+  category: string;
+  features: { name: string }[];
+}
+
+const page = async () => {
+  const page = 1;
+  const limit = 10;
+  try {
+    const res = await fetch(
+      `http://localhost:3000/room/getAllRooms?page=${page}&limit=${limit}`
+    );
+
+    if (!res.ok) {
+      throw new Error("Error al obtener las habitaciones");
+    }
+
+    const data = await res.json();
+
+    // Asegúrate de acceder a la propiedad 'data'
+    const rooms: Room[] = data.data ? data.data : [];
+
+    return (
+      <div className="w-full flex flex-col mt-5 lg:mt-20 p-1 lg:px-32">
+        {rooms.length > 0 ? (
+          rooms.map((room) => (
+            <RoomCard
+              key={room.number}
+              imageUrl="/room.jpg"
+              title={room.category}
+              price={`$${room.price} por noche`}
+              description={`Habitación número ${room.number} con las siguientes características:`}
+              services={room.features.map((feature, index) => ({
+                id: `${room.number}-${index}`,
+                title: feature.name,
+                icon: "wifi",
+              }))}
+            />
+          ))
+        ) : (
+          <p>No hay habitaciones disponibles</p>
+        )}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching rooms:", error);
+    return <p>Hubo un problema al cargar las habitaciones.</p>;
+  }
 };
 
 export default page;

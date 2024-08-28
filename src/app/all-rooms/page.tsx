916e41ea7d924efc59051e1222fdb9ea8fe88f03
indisardi99 <indisardi99@gmail.com>
2024-08-28 12:50:42 -0300

    filters

--- src/app/all-rooms/page.tsx
+++ src/app/all-rooms/page.tsx
@@ -1,5 +1,4 @@
 import FilterDate from "@/components/filter-date/filter-date";
-import PriceFilter from "@/components/filter-price/filter-price";
 import RoomCard from "@/components/room-card/room-card";
 import { Room } from "@/lib/interfaces";
 
@@ -23,9 +22,6 @@ const page = async () => {
       <div className="w-full flex flex-col mt-5 lg:mt-20 p-1 lg:px-32">
         <div className="mb-2 border border-orange-300  rounded-md bg-[#faf9f5] p-2">
           <FilterDate />
-        </div>
-        <div className="m-2 border border-orange-300  rounded-md bg-[#faf9f5] p-2">
-          <PriceFilter />
         </div>
         <div>
           {rooms.length > 0 ? (
--- src/components/filter-date/filter-date.tsx
+++ src/components/filter-date/filter-date.tsx
@@ -14,6 +14,19 @@ import {
 import { CalendarIcon, CheckIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Command, CommandGroup, CommandItem, CommandList } from "../ui/command";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const FilterDate: React.FC = () => {
   const today = new Date();
@@ -32,6 +45,40 @@ const FilterDate: React.FC = () => {
     { value: "2 personas", label: "2 personas" },
     { value: "1 persona", label: "1 persona" },
   ];
+
+  const formSchema = z.object({
+    minPrice: z
+      .string()
+      .min(2, "El precio mínimo debe tener al menos 2 caracteres")
+      .max(50),
+    maxPrice: z
+      .string()
+      .min(2, "El precio máximo debe tener al menos 2 caracteres")
+      .max(50),
+    date: z.any(),
+    guests: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      minPrice: "",
+      maxPrice: "",
+      date: undefined,
+      guests: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    if (date?.from && date?.to) {
+      const startDay = format(date.from, "yyyy-MM-dd");
+      const endDay = format(date.to, "yyyy-MM-dd");
+      router.push(`/search?start=${startDay}&end=${endDay}`);
+    } else {
+      setError("Por favor selecciona un rango de fechas válido.");
+    }
+  }
+
   const handleSelect = (selectedDate: DateRange | undefined) => {
     if (selectedDate?.from && selectedDate?.to) {
       if (selectedDate.from.getTime() === selectedDate.to.getTime()) {
@@ -56,105 +103,153 @@ const FilterDate: React.FC = () => {
     return <span>Check in - Check out</span>;
   };
 
-  // &category=${category}&min=${minPrice}&max=${maxPrice}
-
-  const handleCheckAvailability = () => {
-    if (date?.from && date?.to) {
-      const startDay = format(date.from, "yyyy-MM-dd");
-      const endDay = format(date.to, "yyyy-MM-dd");
-      const category = router.push(`/search?start=${startDay}&end=${endDay}`);
-    } else {
-      setError("Por favor selecciona un rango de fechas válido.");
-    }
-  };
-
   return (
-    <div className=" flex w-full flex-col justify-center lg:flex-row items-center ">
-      <div className=" bg-[#faf9f5] flex flex-col items-start">
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              id="date"
-              variant="outline"
-              className={cn(
-                "flex items-center justify-between p-2 flex-row  bg-[#faf9f5] border border-orange-300  w-72 text-gray-700",
-                !date && "text-muted-foreground"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex w-full flex-col justify-center lg:flex-row items-center">
+          <div className="bg-[#faf9f5] flex flex-col items-start">
+            <FormField
+              control={form.control}
+              name="minPrice"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Precio mínimo</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Mínimo" {...field} />
+                  </FormControl>
+                  <FormDescription>Este es tu precio mínimo.</FormDescription>
+                  <FormMessage />
+                </FormItem>
               )}
-            >
-              <CalendarIcon className="m-2 size-4 " />
-              {renderDateRange()}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent
-            className="flex w-full flex-row items-center justify-center bg-[#faf9f5] p-2 opacity-100"
-            align="start"
-          >
-            <Calendar
-              initialFocus
-              mode="range"
-              defaultMonth={date?.from}
-              selected={date}
-              onSelect={handleSelect}
-              numberOfMonths={2}
-              disabled={[{ before: today }]}
-              classNames={{
-                day_disabled: "text-gray-450",
-                day_range_start: "bg-gray-400 text-black",
-                day_range_middle: "bg-gray-300 text-black",
-                day_range_end: "bg-gray-400 text-black",
-                day_selected: "",
-              }}
             />
-          </PopoverContent>
-        </Popover>
-        {error && <div className="text-xs text-orange-500">{error}</div>}
-      </div>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
+            <FormField
+              control={form.control}
+              name="maxPrice"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Precio máximo</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Máximo" {...field} />
+                  </FormControl>
+                  <FormDescription>Este es tu precio máximo.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          id="date"
+                          variant="outline"
+                          className={cn(
+                            "flex items-center justify-between p-2 flex-row bg-[#faf9f5] border border-orange-300 w-72 text-gray-700",
+                            !date && "text-muted-foreground"
+                          )}
+                        >
+                          <CalendarIcon className="m-2 size-4" />
+                          {renderDateRange()}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent
+                        className="flex w-full flex-row items-center justify-center bg-[#faf9f5] p-2 opacity-100"
+                        align="start"
+                      >
+                        <Calendar
+                          initialFocus
+                          mode="range"
+                          defaultMonth={date?.from}
+                          selected={date}
+                          onSelect={(selected) => {
+                            handleSelect(selected);
+                            field.onChange(selected);
+                          }}
+                          numberOfMonths={2}
+                          disabled={[{ before: today }]}
+                          classNames={{
+                            day_disabled: "text-gray-450",
+                            day_range_start: "bg-gray-400 text-black",
+                            day_range_middle: "bg-gray-300 text-black",
+                            day_range_end: "bg-gray-400 text-black",
+                            day_selected: "",
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  {error && <FormMessage>{error}</FormMessage>}
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name="guests"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        aria-expanded={open}
+                        className="m-4 w-72 bg-[#faf9f5] border border-orange-300 p-2 text-gray-700 justify-between"
+                      >
+                        {guests || "Selecciona los huéspedes"}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                      <Command>
+                        <CommandList>
+                          <CommandGroup>
+                            {guestOptions.map((option) => (
+                              <CommandItem
+                                key={option.value}
+                                value={option.value}
+                                onSelect={(currentValue) => {
+                                  setGuests(
+                                    currentValue === guests ? "" : currentValue
+                                  );
+                                  setOpen(false);
+                                  field.onChange(currentValue);
+                                }}
+                              >
+                                {option.label}
+                                <CheckIcon
+                                  className={cn(
+                                    "ml-auto h-4 w-4",
+                                    guests === option.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                </FormControl>
+              </FormItem>
+            )}
+          />
           <Button
+            className="m-2 bg-[#faf9f5] border border-orange-300 p-2 text-gray-700"
             variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="m-4 w-72 bg-[#faf9f5] border border-orange-300 p-2 text-gray-700 justify-between"
+            type="submit"
           >
-            {guests || "Selecciona los huéspedes"}
+            Revisar Disponibilidad
           </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandList>
-              <CommandGroup>
-                {guestOptions.map((option) => (
-                  <CommandItem
-                    key={option.value}
-                    value={option.value}
-                    onSelect={(currentValue) => {
-                      setGuests(currentValue === guests ? "" : currentValue);
-                      setOpen(false);
-                    }}
-                  >
-                    {option.label}
-                    <CheckIcon
-                      className={cn(
-                        "ml-auto h-4 w-4",
-                        guests === option.value ? "opacity-100" : "opacity-0"
-                      )}
-                    />
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      <Button
-        className="m-2  bg-[#faf9f5] border border-orange-300 p-2 text-gray-700"
-        variant="outline"
-        onClick={handleCheckAvailability}
-      >
-        Revisar Disponibilidad
-      </Button>
-    </div>
+        </div>
+      </form>
+    </Form>
   );
 };
 

58fe5a4892691c3376036f2ba361d4d5be40ccc5
indisardi99 <indisardi99@gmail.com>
2024-08-30 09:47:42 -0300

    debug

--- src/app/all-rooms/page.tsx
+++ src/app/all-rooms/page.tsx
@@ -1,55 +1,32 @@
-// import FilterDate from "@/components/filter-date/filter-date";
-// import RoomCard from "@/components/room-card/room-card";
-// import { Room } from "@/lib/interfaces";
+import FilterDate from "@/components/filter-date/filter-date";
 
-// const page = async () => {
-//   const page = 1;
-//   const limit = 10;
-//   try {
-//     const res = await fetch(
-//       `http://localhost:3000/room/getAllRooms?page=${page}&limit=${limit}`,
-//       { next: { revalidate: 1 } }
-//     );
+const page = async () => {
+  const page = 1;
+  const limit = 10;
+  try {
+    const res = await fetch(
+      `http://localhost:3000/room/getAllRooms?page=${page}&limit=${limit}`,
+      { next: { revalidate: 1 } }
+    );
 
-//     if (!res.ok) {
-//       throw new Error("Error al obtener las habitaciones");
-//     }
+    if (!res.ok) {
+      throw new Error("Error al obtener las habitaciones");
+    }
 
-//     const data = await res.json();
-//     const rooms: Room[] = data.data ? data.data : [];
+    const data = await res.json();
 
-//     return (
-//       <div className="w-full flex flex-col mt-5 lg:mt-2 p-1 ">
-//         <div className=" border border-orange-300  rounded-md bg-[#faf9f5] ">
-//           <FilterDate />
-//         </div>
-//         <div>
-//           {rooms.length > 0 ? (
-//             rooms.map((room) => (
-//               <RoomCard
-//                 key={room.id}
-//                 id={room.id}
-//                 number={room.number}
-//                 imageUrl="/room.jpg"
-//                 title={room.category}
-//                 price={room.price}
-//                 services={room.features.map((feature, index) => ({
-//                   id: `${room.number}-${index}`,
-//                   title: feature.name,
-//                   icon: "",
-//                 }))}
-//               />
-//             ))
-//           ) : (
-//             <p>No hay habitaciones disponibles</p>
-//           )}
-//         </div>
-//       </div>
-//     );
-//   } catch (error) {
-//     console.error("Error fetching rooms:", error);
-//     return <p>Hubo un problema al cargar las habitaciones.</p>;
-//   }
-// };
+    return (
+      <div className="w-full flex flex-col mt-5 lg:mt-2 p-1 ">
+        <div className=" border border-orange-300  rounded-md bg-[#faf9f5] ">
+          <FilterDate />
+        </div>
+        <h1>Habitaciones</h1>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching rooms:", error);
+    return <p>Hubo un problema al cargar las habitaciones.</p>;
+  }
+};
 
-// export default page;
+export default page;

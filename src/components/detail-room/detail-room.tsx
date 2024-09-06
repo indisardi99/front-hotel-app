52093c1cc764d6027743e50db032876498e69c8e
indisardi99 <indisardi99@gmail.com>
2024-09-06 19:00:34 -0300

    details-room

--- src/components/detail-room/detail-room.tsx
+++ src/components/detail-room/detail-room.tsx
@@ -93,93 +93,90 @@ const RoomDetails: React.FC<{
   };
 
   return (
-    <div className="flex flex-col justify-around lg:flex-row w-full mb-4 rounded-lg bg-[#faf9f5] border border-orange-300 p-4">
-      <div className="flex-row ">
-        <div className="flex flex-row">
-          <div className="flex flex-col">
-            <div className="flex mt-2 flex-col justify-center items-center">
-              {room?.images && room.images.length > 0 && (
-                <Carousel className="w-full max-w-md">
-                  <CarouselContent>
-                    {room.images.slice(0, 3).map((image, index) => (
-                      <CarouselItem key={index}>
-                        <div className="p-1">
-                          <Image
-                            src={image}
-                            alt={`${room.category} image ${index + 1}`}
-                            width={600}
-                            height={600}
-                            className="rounded-lg object-cover"
-                          />
-                        </div>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious />
-                  <CarouselNext />
-                </Carousel>
-              )}
-            </div>
-            <div className="flex w-[500px]">
-              <Guests category={room.category} />
+    <div className="flex flex-col justify-around lg:flex-row w-full mb-4 rounded-lg bg-[#faf9f5] border border-orange-300 p-2">
+      <div className="flex flex-col">
+        <div className="flex mt-5 flex-col justify-center items-center">
+          {room?.images && room.images.length > 0 && (
+            <Carousel className="lg:w-[800px]">
+              <CarouselContent>
+                {room.images.slice(0, 3).map((image, index) => (
+                  <CarouselItem key={index}>
+                    <div className="">
+                      <Image
+                        src={image}
+                        alt={`${room.category} image ${index + 1}`}
+                        width={800}
+                        height={0}
+                        className="rounded-lg object-cover"
+                      />
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          )}
+        </div>
+        <div className="flex flex-row ml-3 gap-x-5 mt-5">
+          <h2 className="text-xl font-bold uppercase">{room.category}</h2>
+          <p className="text-xl text-black font-semibold">
+            ${room.price} precio por noche.
+          </p>
+          <h2 className="text-xl font-semibold text-gray-600">
+            Habitación Nro. {room.number}
+          </h2>
+          <p className="text-xl text-gray-600">
+            {getCapacityMessage(room.category)}
+          </p>
+        </div>
+        <div className="flex flex-col ml-3 mt-3 gap-3">
+          <h3 className="text-xl font-semibold">
+            Características incluidas en la habitacion:
+          </h3>
+          <div className="mb-4 flex flex-wrap gap-2">
+            {room.features.map((feature: Feature) => (
+              <div
+                key={feature.id}
+                className="flex size-20 flex-col items-center rounded-md border text-black"
+              >
+                {featureIconMap[feature.name] || (
+                  <Leaf className="m-2 size-4" />
+                )}{" "}
+                <h1 className="mt-2 text-xs">{feature.name}</h1>
+              </div>
+            ))}
+          </div>
+          <div className="w-full">
+            <h3 className="text-lg font-semibold">
+              Puedes añadir servicios adicionales:
+            </h3>
+            <h3 className="text-xs text-gray-500">
+              Puedes incluir en tu estadia servicios diarios para disfrutar mas
+              de nuestras instalaciones.
+            </h3>
+            <div className="mb-4 flex flex-wrap gap-2">
+              {services.map((service: Service) => (
+                <div
+                  className={`cursor-pointer rounded-lg ${
+                    isSelected(service.name)
+                      ? "bg-orange-300 hover:translate-y-2 transition-all duration-300 shadow-lg py-2 px-3  mt-2"
+                      : "bg-white shadow-lg py-2 px-3 mt-2 hover:translate-y-2 transition-all duration-300"
+                  }`}
+                  key={service.id}
+                  onClick={() => handleServiceClick(service)}
+                >
+                  <ServiceCard
+                    name={service.name}
+                    price={service.price}
+                    icon={getServiceIcon(service.name)}
+                  />
+                </div>
+              ))}
             </div>
           </div>
-          <div className="ml-5 flex flex-1 flex-col gap-5">
-            <div className="mt-5 flex flex-col gap-5">
-              <h2 className="text-xl font-semibold uppercase">
-                {room.category}
-              </h2>
-              <p className="text-lg text-black font-semibold">${room.price}</p>
-              <p className="text-lg text-gray-600">
-                {getCapacityMessage(room.category)}
-              </p>
-              <h2 className="text-xl font-semibold text-gray-600">
-                Habitación Nro. {room.number}
-              </h2>
-            </div>
-            <div className="flex flex-col gap-3">
-              <h3 className="text-lg font-semibold">Características:</h3>
-              <div className="mb-4 flex flex-wrap gap-2">
-                {room.features.map((feature: Feature) => (
-                  <div
-                    key={feature.id}
-                    className="flex size-20 flex-col items-center rounded-md border text-black"
-                  >
-                    {featureIconMap[feature.name] || (
-                      <Leaf className="m-2 size-4" />
-                    )}{" "}
-                    <h1 className="mt-2 text-xs">{feature.name}</h1>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="w-full">
-              <h3 className="text-lg font-semibold">
-                Puedes añadir servicios adicionales:
-              </h3>
-              <h3 className="text-xs text-gray-500">
-                Se agrega a tu habitacion diariamente.
-              </h3>
-              <div className="mb-4 flex flex-wrap gap-2">
-                {services.map((service: Service) => (
-                  <div
-                    className={`cursor-pointer rounded-lg ${
-                      isSelected(service.name)
-                        ? "bg-orange-300 hover:translate-y-2 transition-all duration-300 shadow-lg py-2 px-3  mt-2"
-                        : "bg-white shadow-lg py-2 px-3 mt-2 hover:translate-y-2 transition-all duration-300"
-                    }`}
-                    key={service.id}
-                    onClick={() => handleServiceClick(service)}
-                  >
-                    <ServiceCard
-                      name={service.name}
-                      price={service.price}
-                      icon={getServiceIcon(service.name)}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
+          <div className="flex mt-3 w-[500px]">
+            <Guests category={room.category} />
           </div>
         </div>
       </div>
